a1614dfa1714c6f4d34e5e94a0bb004b28e60bc4
José Humberto <jhpnascimento96@gmail.com>
2022-02-25 10:54:36 -0300

    created server

--- apiBootcampEduzz/src/index.ts
+++ apiBootcampEduzz/src/index.ts
@@ -1,1 +1,15 @@
-console.log("ss");
+import Express, {Request, Response, NextFunction} from "express";
+
+
+
+
+const app = Express();
+
+app.get('/status', (request: Request, response: Response, next: NextFunction) => {
+    response.status(200).send({ foo: 'testando'});
+});
+
+app.listen(3000, () => {
+    console.log("Servidor iniciado na porta 3000: http://localhost:3000");
+    
+});

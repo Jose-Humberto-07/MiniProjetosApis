ac8d8d544a604e2686f3ee3ee2fc2c37ad27c052
José Humberto <jhpnascimento96@gmail.com>
2022-02-25 15:21:22 -0300

    operations basic crud

--- apiBootcampEduzz/src/index.ts
+++ apiBootcampEduzz/src/index.ts
@@ -1,14 +1,24 @@
-import Express, {Request, Response, NextFunction} from "express";
+import express, {Request, Response, NextFunction} from "express";
+import userRoute from "./routes/users.route";
 
 
 
+const app = express();
 
-const app = Express();
+// configurações da apliacação
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+// consfigurações das rotas
+app.use(userRoute);
 
 app.get('/status', (request: Request, response: Response, next: NextFunction) => {
-    response.status(200).send({ foo: 'testando'});
+    response.status(200).send({ foo: 'testandoooooooooooooooo'});
 });
 
+
+// servidor http
 app.listen(3000, () => {
     console.log("Servidor iniciado na porta 3000: http://localhost:3000");
     
---
+++ apiBootcampEduzz/src/routes/users.route.ts
@@ -0,0 +1,47 @@
+import {NextFunction, Request, Response, Router} from 'express';
+import { StatusCodes } from 'http-status-codes';
+
+
+// get /users
+// get /users/:uuid
+// post /users
+// put /users/:uuid
+// delete /users/:uuid
+
+const userRoute = Router();
+// listar todos os usuários
+userRoute.get('/users', (request: Request, response: Response, next: NextFunction) => {
+    const users = [{
+        userName: 'Humberto'
+    }];
+    response.status(StatusCodes.OK).send(users);
+});
+// listar 1 usuário passando o ID por parâmetro
+userRoute.get('/users/:uuid', (request: Request<{ uuid: string }>, response: Response, next: NextFunction) => {
+    const uuid = request.params.uuid;
+    response.status(200).send({ uuid });
+});
+// insereir um novo usuário
+userRoute.post('/users', (request: Request, response: Response, next: NextFunction) => {
+    const newUser = request.body;
+
+    console.log(request.body)
+
+    response.status(StatusCodes.CREATED).send(newUser);
+});
+// atualizando um usuário
+userRoute.put('/users/:uuid', (request: Request<{ uuid: string }>, response: Response, next: NextFunction) => {
+    const uuid = request.params.uuid;
+    const modifieUser = request.body;
+
+    modifieUser.uuid = uuid;
+
+    response.status(StatusCodes.OK).send({ modifieUser });
+});
+// deletando um usuário
+userRoute.delete('/users/:uuid', (request: Request<{ uuid: string }>, response: Response, next: NextFunction) => {
+
+    response.sendStatus(StatusCodes.OK);
+});
+
+export default userRoute;

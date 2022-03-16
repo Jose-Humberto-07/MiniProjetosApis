a2be14b482e737ea8af96396aba8b614c9f6d13e
José Humberto <jhpnascimento96@gmail.com>
2022-03-16 17:31:14 -0300

    exceptions

---
+++ jsAvancedAndTs/tratamento.js
@@ -0,0 +1,25 @@
+class CustomError extends Error {
+    constructor({ message, data}) {
+        super(message);
+        this.data = data;
+    }
+}
+
+try {
+    
+    const name = "José Humberto Pinheiro do Nascimento";
+
+    const myError = new CustomError({
+        message: "Custom message on custom error",
+        data: {
+            type: "Server error"
+        }
+    }); 
+    throw myError;
+} catch (error) {
+    console.log("Error: ", error);
+    console.log(error);
+    console.log(error.data);
+} finally {
+    console.log("Keep going...");
+}
